308567835362408ebf5e8cd5c6be3db2f451ac7f
Shiton <shiltonrons@gmail.com>
2024-05-09 18:34:20 +0530

    second updated on 5/9

--- src/main/java/com/example/Music/Instrumental/Store/Repository/AddressRepository.java
+++ src/main/java/com/example/Music/Instrumental/Store/Repository/AddressRepository.java
@@ -1,7 +1,9 @@
 package com.example.Music.Instrumental.Store.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.Music.Instrumental.Store.Model.Address;
+@Repository
 
 public interface AddressRepository extends JpaRepository<Address, Integer>{
 
